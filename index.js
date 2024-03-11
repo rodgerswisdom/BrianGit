030ba5ee8c1a5520d4f2a361a2a08e70321f0f7f
rodgerswisdom <trulyhawona@gmail.com>
2024-03-11 13:07:32 +0300

    calculator

--- index.js
+++ index.js
@@ -16,4 +16,20 @@ myname = 5 */
 
 // console.log(myname);
 
+let a, b;
+
+a = 6
+b=4
+
+let sum = a+b;
+let product = a*b;
+let quotient = a/b;
+let subtraction = a -b;
+
+// console.log("\n","sum:",sum, "\n","product:",product, "\n", "Division:", quotient, "\n","subtraction:", subtraction);
+
+console.log("\nsum:", sum);
+console.log("\nproduct:", product);
+console.log("\nDivision:", quotient);
+console.log("\nsubtraction:", subtraction);
 
