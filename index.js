55c2e011fc448cb6158f4dacdf69073433b77409
rodgerswisdom <trulyhawona@gmail.com>
2024-03-11 13:20:25 +0300

    Sum

--- index.js
+++ index.js
@@ -16,20 +16,33 @@ myname = 5 */
 
 // console.log(myname);
 
-let a, b;
+// let a, b;
 
-a = 6
-b=4
+// a = 6
+// b=4
 
-let sum = a+b;
-let product = a*b;
-let quotient = a/b;
-let subtraction = a -b;
+// let sum = a+b;
+// let product = a*b;
+// let quotient = a/b;
+// let subtraction = a -b;
 
 // console.log("\n","sum:",sum, "\n","product:",product, "\n", "Division:", quotient, "\n","subtraction:", subtraction);
 
-console.log("\nsum:", sum);
-console.log("\nproduct:", product);
-console.log("\nDivision:", quotient);
-console.log("\nsubtraction:", subtraction);
+// console.log("\nsum:", sum);
+// console.log("\nproduct:", product);
+// console.log("\nDivision:", quotient);
+// console.log("\nsubtraction:", subtraction);
 
+// function multiply(num1, num2) {
+//     let result = num1 * num2;
+//     console.log(result);
+//     // return result;
+//   }
+
+// multiply(5, 8)
+
+function Sum(a,b){
+    console.log("Sum is:", a+b);
+}
+
+Sum(5, 5);
