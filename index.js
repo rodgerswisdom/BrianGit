3efcea240f13c34f8336cc7e30a258195426f282
rodgerswisdom <trulyhawona@gmail.com>
2024-03-11 12:51:07 +0300

    Learn js

---
+++ index.js
@@ -0,0 +1,19 @@
+// let myname = "Rodgers";
+
+/* myname = "Hawona";
+
+
+
+myname = 5 */
+
+// let mynum = "8";
+// if (mynum === 5){
+//     console.log('True');
+// }
+// else{
+//     console.log('False');
+// }
+
+// console.log(myname);
+
+
