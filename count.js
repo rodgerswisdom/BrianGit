2ad8eaf23639179c53740b690ef86e2f960dcbe5
rodgerswisdom <trulyhawona@gmail.com>
2024-03-12 18:09:42 +0300

    JS intro

--- count.js
+++ count.js
@@ -1,11 +1,25 @@
-function generateNumberPattern(num){
-    for (i=1; i<=num; i++){
-        let row='';
-        for (j=1; j<=i; j++){
-            row += i;
+// function generateNumberPattern(num){
+//     for (i=1; i<=num; i++){
+//         let row='';
+//         for (j=1; j<=i; j++){
+//             row += i;
+//         }
+//     console.log(row)
+//     }
+// }
+
+// generateNumberPattern(5)
+
+function count(num){
+    for(i=1; i<=num; i++){
+        patt = ""
+
+        for(j=1; j<=i; j++){
+           patt+= i
+           
         }
-    console.log(row)
+    console.log(patt)
     }
 }
 
-generateNumberPattern(5)
+count(5)
